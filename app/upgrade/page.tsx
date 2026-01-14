1d9fbe84866a97b98cbe6abb061d7edea33f548f
Manoj Kumar <ms8460149@gmail.com>
2026-01-14 15:50:13 +0530

    fix: upgrade page

--- app/upgrade/page.tsx
+++ app/upgrade/page.tsx
@@ -1,9 +1,15 @@
+import { ClientHeaderWrapper } from "@/components/ClientHeaderWrapper";
+import Footer from "@/components/Footer";
 import Pricing from "@/components/Pricing";
 
 export default function UpgradePage() {
   return (
-    <div className="min-h-screen bg-background">
-      <Pricing />
+    <div className="flex min-h-screen flex-col bg-background">
+      <ClientHeaderWrapper />
+      <main className="flex-1">
+        <Pricing />
+      </main>
+      <Footer />
     </div>
   );
 }
--- components/get-pro-cta.tsx
+++ components/get-pro-cta.tsx
@@ -6,7 +6,7 @@ import { cn } from "@/lib/utils";
 import { Gem } from "lucide-react";
 import Link from "next/link";
 
-interface GetProCTAProps extends React.ComponentProps<typeof Button> {}
+interface GetProCTAProps extends React.ComponentProps<typeof Button> { }
 
 export function GetProCTA({ className, ...props }: GetProCTAProps) {
   const { isLoading, isPaidUser } = useTrialStatus();
